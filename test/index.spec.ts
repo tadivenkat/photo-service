038b2ee13ab967bce74f0b69ab00b43b320c7c78
Venkat Ram Taddi <tadivenkat@gmail.com>
2024-11-16 21:43:21 -0600

    Added unit tests for GET /images and GET /images/:id endpoints

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,6 +2,7 @@
 import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
 import worker from '../src/index';
+import IImage from '../src/data/iimage';
 
 // For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
@@ -27,11 +28,34 @@ describe('Photo-Service worker', () => {
 		const response = await SELF.fetch('https://example.com');
 		expect(await response.text()).toMatchInlineSnapshot(`"Welcome to Photo Service!"`);
 	});
-	
+
 	describe("GET /images", () => {
 		it("should return 200 status code", async () => {
 			const response = await SELF.fetch("https://example.com/images");
 			expect(response.status).toEqual(200);
+		});
+		it("should return images size equal to count query param", async () => {
+			const response = await SELF.fetch("https://example.com/images?count=2");
+			const images : IImage[] = await response.json();
+			expect(images.length).toEqual(2);
+		});
+		it("should return all images", async () => {
+			const response = await SELF.fetch("https://example.com/images");
+			const images : IImage[] = await response.json();
+			expect(images.length).toEqual(3);
+		});
+		it("should return images containing an image with id 1", async () => {
+			const response = await SELF.fetch("https://example.com/images");
+			const images : IImage[] = await response.json();
+			expect(images.find(image => image.id == 1)).not.toBeNull();
+		});
+	});
+
+	describe("GET /images/:id", () => {
+		it("should return an image with id 1", async () => {
+			const response = await SELF.fetch("https://example.com/images/1");
+			const image : IImage = await response.json();
+			expect(image.id).toEqual(1);
 		});
 	});
 });
