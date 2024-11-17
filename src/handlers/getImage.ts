1fbee3c517dfa4120418507c5310956a668f2a1f
Venkat Ram Taddi <tadivenkat@gmail.com>
2024-11-16 20:09:57 -0600

    Refactoring...

--- src/handlers/getImage.ts
+++ src/handlers/getImage.ts
@@ -4,10 +4,10 @@ import images from "../data/images";
 const getmage = (request: IRequest) => {
     const id = request.params.id;
     const image = images.find(image => image.id == parseInt(id));
-    if (image) {
-        return new Response(JSON.stringify(image), { headers: { "content-type": "application/json" }, status: 200 });
+    if (!image) {
+        return new Response("Image not found", { status: 404 });
     }
-    return new Response("Image not found", { status: 404 });
+    return new Response(JSON.stringify(image), { headers: { "content-type": "application/json" }, status: 200 });
 }
 
 export default getmage;
